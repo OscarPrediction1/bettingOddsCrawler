1dda65b6bfc5e3b557df99330c54009b656bae54
sebastiankrauss <seb_939@gmx.net>
2015-10-23 10:00:26 +0200

    fixed db authentication

--- de.coins2015.oscar1.bettingoddscrawler/src/main/java/de/coins2015/oscar1/bettingoddscrawler/MongoDBHandler.java
+++ de.coins2015.oscar1.bettingoddscrawler/src/main/java/de/coins2015/oscar1/bettingoddscrawler/MongoDBHandler.java
@@ -1,5 +1,7 @@
 package de.coins2015.oscar1.bettingoddscrawler;
 
+import java.util.Arrays;
+
 import org.json.simple.JSONObject;
 
 import com.mongodb.DB;
@@ -7,6 +9,7 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.util.JSON;
 
 public class MongoDBHandler {
@@ -19,7 +22,9 @@ public class MongoDBHandler {
 	    String database, String password) {
 	this.mongoCredential = MongoCredential.createCredential(userName,
 		database, password.toCharArray());
-	this.mongoClient = new MongoClient(host, Integer.parseInt(port));
+
+	this.mongoClient = new MongoClient(new ServerAddress(Controller.host),
+		Arrays.asList(mongoCredential));
 	this.db = mongoClient.getDB(database);
     }
 
